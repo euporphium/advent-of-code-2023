806e280112ec5c02e326458485123de858ac2fe5
Daniel Newton <kdaniel.newton@gmail.com>
2023-12-05 23:19:42 -0600

    Add solution for day 3

---
+++ solutions/js/day-3/gear-ratios.js
@@ -0,0 +1,98 @@
+import { promises as fs } from 'fs';
+
+// const input = await fs.readFile('../../../input/day-3/example.txt', 'utf8');
+const input = await fs.readFile('../../../input/day-3/challenge.txt', 'utf8');
+
+const isNumber = (value) => !isNaN(parseInt(value));
+
+const lines = input.split('\n');
+
+// Part 1
+// function isSymbol(l, p) {
+//   const isInRange = 0 <= l && l < lines.length && 0 <= p && p < lines[l].length;
+//   return isInRange && lines[l][p] && lines[l][p] !== '.';
+// }
+
+// let solution = 0;
+
+// for (let l = 0; l < lines.length; l++) {
+//   let found = '';
+//
+//   for (let p = 0; p <= lines[l].length; p++) {
+//     const curr = lines[l][p];
+//     if (isNumber(curr)) {
+//       found += curr;
+//     } else {
+//       if (found.length > 0) {
+//         const leftBound = p - found.length - 1;
+//         if (isSymbol(l, leftBound) || isSymbol(l, p)) {
+//           solution += parseInt(found);
+//         } else {
+//           for (let x = leftBound; x <= p; x++) {
+//             if (isSymbol(l - 1, x) || isSymbol(l + 1, x)) {
+//               solution += parseInt(found);
+//               break;
+//             }
+//           }
+//         }
+//         found = '';
+//       }
+//     }
+//   }
+// }
+
+// Part 2
+const gearMap = new Map();
+
+function search(l, p, part) {
+  const key = `${l}-${p}`;
+  const isInRange = 0 <= l && l < lines.length && 0 <= p && p < lines[l].length;
+
+  if (!isInRange) {
+    return false;
+  }
+
+  if (lines[l][p] === '*') {
+    if (gearMap.has(key)) {
+      const { parts, count } = gearMap.get(key);
+      gearMap.set(key, { parts: [...parts, part], count: count + 1 });
+    } else {
+      gearMap.set(key, { parts: [part], count: 1 });
+    }
+  }
+
+  return isInRange && lines[l][p] !== '.';
+}
+
+for (let l = 0; l < lines.length; l++) {
+  let found = '';
+
+  for (let p = 0; p <= lines[l].length; p++) {
+    const curr = lines[l][p];
+    if (isNumber(curr)) {
+      found += curr;
+    } else {
+      if (found.length > 0) {
+        const leftBound = p - found.length - 1;
+        search(l, leftBound, found);
+        search(l, p, found);
+
+        for (let x = leftBound; x <= p; x++) {
+          search(l - 1, x, found);
+          search(l + 1, x, found);
+        }
+
+        found = '';
+      }
+    }
+  }
+}
+
+let sum = 0;
+for (const { parts, count } of gearMap.values()) {
+  if (count === 2) {
+    sum += parts[0] * parts[1];
+  }
+}
+
+console.log(sum);
